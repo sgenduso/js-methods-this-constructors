0035bc4e509c59db231e8286d719a2f18bec0ba5
Sam Genduso <sam.genduso@gmail.com>
2015-07-13 16:11:42 -0600

    all taxi tests pass

--- spec/taxi_spec.js
+++ spec/taxi_spec.js
@@ -3,11 +3,11 @@ var Taxi = require('../taxi');
 describe('properties', function() {
   var cab = new Taxi("Phil Collins", 6);
 
-  xit('has a String driverName', function() {
+  it('has a String driverName', function() {
     expect(cab.driverName).toEqual('Phil Collins');
   });
 
-  xit('has an integer maxNumberOfPassengers', function() {
+  it('has an integer maxNumberOfPassengers', function() {
     expect(cab.maxNumberOfPassengers).toEqual(6);
   });
 });
@@ -19,12 +19,12 @@ describe('#addPassenger', function() {
     cab = new Taxi("Phil Collins", 6);
   });
 
-  xit('takes a String argument for the passengerName', function() {
+  it('takes a String argument for the passengerName', function() {
     cab.addPassenger('Tom Collins');
     expect(cab.passengers).toEqual(['Tom Collins']);
   });
 
-  xit('correctly handles multiple passengers', function() {
+  it('correctly handles multiple passengers', function() {
     cab.addPassenger('Tom Collins');
     cab.addPassenger('Bob Collins');
     expect(cab.passengers).toEqual(['Tom Collins', 'Bob Collins']);
@@ -32,7 +32,7 @@ describe('#addPassenger', function() {
 });
 
 describe('#passengerCount', function() {
-  xit('returns the number of passengers in the Taxi instance', function() {
+  it('returns the number of passengers in the Taxi instance', function() {
     var cab = new Taxi('Ringo Starr', 4);
     cab.addPassenger('Paul McCartney');
     cab.addPassenger('John Lennon');
@@ -41,7 +41,7 @@ describe('#passengerCount', function() {
     expect(cab.passengerCount()).toEqual(3);
   });
 
-  xit('returns 0 when there are no passengers in the Taxi instance', function() {
+  it('returns 0 when there are no passengers in the Taxi instance', function() {
     var cab = new Taxi('Ringo Starr', 4);
 
     expect(cab.passengerCount()).toEqual(0);
@@ -58,19 +58,19 @@ describe('#full', function() {
     cab.addPassenger('George Harrison');
   });
 
-  xit('returns true when the passengerCount is at the maximum number of passengers', function() {
+  it('returns true when the passengerCount is at the maximum number of passengers', function() {
     cab.addPassenger('Yoko Ono');
 
     expect(cab.full()).toBe(true);
   });
 
-  xit('returns false when there are less passengers than there is space for passengers', function() {
+  it('returns false when there are less passengers than there is space for passengers', function() {
     expect(cab.full()).toBe(false);
   });
 });
 
 describe('#dropOffPassengers', function() {
-  xit('removes all stored passengers from the Taxi instance', function() {
+  it('removes all stored passengers from the Taxi instance', function() {
     var cab = new Taxi('Ringo Starr', 4);
     cab.addPassenger('Paul McCartney');
     cab.addPassenger('John Lennon');
--- taxi.js
+++ taxi.js
@@ -1,12 +1,28 @@
 function Taxi(driverName, maxNumberOfPassengers) {
-  // your code here
+  this.driverName = driverName;
+  this.maxNumberOfPassengers = maxNumberOfPassengers;
+  this.passengers = [];
 }
 
 Taxi.prototype.addPassenger = function(passengerName) {
-  // your code here
+  this.passengers.push(passengerName);
 };
 
-// your code here
+Taxi.prototype.passengerCount = function () {
+  return this.passengers.length;
+};
+
+Taxi.prototype.full = function () {
+  if (this.passengers.length === this.maxNumberOfPassengers) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Taxi.prototype.dropOffPassengers = function () {
+  this.passengers.length = 0;
+};
 
 // DO NOT MODIFY BELOW THIS COMMENT:
 module.exports = Taxi;
